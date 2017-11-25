ed7ebda5b4ae026f7dcc09a01907e4254d1d233d
farzansalimian <farzan_salimiyan@yahoo.com>
2017-11-25 23:30:27 +0330

    fifteenth update

--- acm/settings.py
+++ acm/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '5-&cjq_yuc&&a*3d_k2m$_b*etgt$_v*4+t=-c+a1$b*!%wluv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1']
+ALLOWED_HOSTS = ['acm.ui.ac.ir']
 
 
 # Application definition
